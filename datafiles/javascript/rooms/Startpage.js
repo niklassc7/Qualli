d34a9871e79ae24e67dbf3db99595fed4432b0d8
niklassc <niklassc@student.ethz.ch>
2024-12-15 21:01:34 +0100

    add blur animation for title

--- datafiles/javascript/rooms/Startpage.js
+++ datafiles/javascript/rooms/Startpage.js
@@ -5,7 +5,7 @@ class Startpage extends Room {
 		document.body.style.backgroundImage = spr_bgMenu
 
 
-		this.n_step = 0;
+		this.n_step = 0; // TODO is this needed
 		/*
 		* Erstelle Objekte und trage diese in das Array objlist,
 		* durch die Funktion count_Objekt, ein
@@ -31,7 +31,22 @@ class Startpage extends Room {
 		ctx.textAlign = "center";
 		ctx.lineWidth = 8 * ((xScalar + yScalar) / 2);
 		let fill = 0.9
+
+		let animationN = 128
+		let animationSpeed = 0.2
+		let blur = stepCount * animationSpeed % animationN
+		blur = Math.abs(blur - animationN/2)
+		// Normalize
+		blur = blur / (animationN/2)
+		// Scale
+		blur = 5 + 20*blur
+
+		ctx.filter = `blur(${blur}px)`;
+		// ctx.filter = "drop-shadow(0 0 0.75rem white)";
 		ctx.strokeStyle = `rgba(210, 230, 255, ${fill})`;
+		ctx.strokeText("Qualli", roomWidth/2 * xScalar, 148 * yScalar);
+		ctx.filter = "none";
+
 		ctx.strokeText("Qualli", roomWidth/2 * xScalar, 148 * yScalar);
 		// ctx.fillText("Qualli", roomWidth/2 * xScalar, 148 * yScalar);
 
