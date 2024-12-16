3089f97d0ac10865d2d903634f34d923278f80e4
niklassc <niklassc@student.ethz.ch>
2024-12-16 21:46:44 +0100

    fix elements in planetlist not being consecutive

--- datafiles/javascript/rooms/room24.js
+++ datafiles/javascript/rooms/room24.js
@@ -15,6 +15,7 @@ class room24 extends LevelRoom {
 		let rowWidth = (itemsInRow-1) * planetDistance;
 		let startMargin = (roomWidth - rowWidth) / 2;
 
+		let amount = 10
 		for(let i = 0; i < 3; i++)
 			for(let j = 0; j < itemsInRow; j++) {
 				if (j > 2) {
@@ -25,12 +26,15 @@ class room24 extends LevelRoom {
 				let newP = this.addToObjList(new cls_Planet(startMargin + j * planetDistance, 160 + i * planetDistance, newTeam));
 				// newP.groesse = (i == 1) ? 2 : 1;
 				newP.groesse = 1;
-				newP.einheiten = 30;
-				this.planetlist[i * itemsInRow + j] = newP;
+				newP.einheiten = amount;
+				this.planetlist.push(newP)
+				// this.planetlist[i * itemsInRow + j] = newP;
 
 			}
 
 		let newP = this.addToObjList(new cls_Planet(startMargin + 4 * planetDistance, 160 + 1 * planetDistance, 2));
+		newP.einheiten = 9.5 * amount;
 		newP.groesse = 8;
+		this.planetlist.push(newP);
 	}
 }
