9329ce82261b5f9cae69f535d5cd60b60b13286e
niklassc <niklassc@student.ethz.ch>
2025-01-06 22:11:11 +0100

    add getMix method

--- datafiles/javascript/appEtc/color/Color.js
+++ datafiles/javascript/appEtc/color/Color.js
@@ -6,6 +6,17 @@ class Color {
 		this.a = a;
 	}
 
+	// Returns new color that is a mix of `this` and `color`.
+	// Weight `w` (between 0 and 1) determines how much `color` is weighted.
+	getMix(color, w=0.5) {
+		let iw = 1.0 - w;
+		let r = this.r*iw + color.r*w
+		let g = this.g*iw + color.g*w
+		let b = this.b*iw + color.b*w
+
+		return new Color(r, g, b);
+	}
+
 	rgb() {
 		return [this.r, this.g, this.b];	
 	}
