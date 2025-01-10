c29f23c015e6d6fbab23b630ec42c36c24387227
niklassc <niklassc@student.ethz.ch>
2025-01-11 00:46:39 +0100

    add passive ai KI3

---
+++ datafiles/javascript/appEtc/KI3.js
@@ -0,0 +1,29 @@
+import KI from "./KI.js";
+
+// Passive
+
+export default class KI3 extends KI {
+	constructor(team) {
+		super(team);
+
+		this.alarm = [];
+		this.alarm[0] = 20;
+	}
+
+	step() {
+		super.step();
+	}
+
+	alarmieren(nr) {
+		switch(nr) {
+			case 0:
+				if(this.deleteIfDefeatedAndCheckIfWon())
+					return;
+
+				this.alarm[0] = 200 + Math.round(Math.random() * 100);
+				break;
+			default:
+				console.log("Fehler: alarm() von KI: alarm() aufgerufen ohne nr");
+		}
+	}
+}
