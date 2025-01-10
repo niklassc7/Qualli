b7392155cb2b7b6dfa0c1bbbc2de650c6dda7dab
niklassc <niklassc@student.ethz.ch>
2025-01-11 00:41:24 +0100

    dont send jellies from busy bubbles

--- datafiles/javascript/appEtc/aiModules/ModDefend.js
+++ datafiles/javascript/appEtc/aiModules/ModDefend.js
@@ -1,18 +1,28 @@
-import * as g from "../../globals.js";
 import * as f from "../../functions.js";
+import * as g from "../../globals.js";
 import AiModule from "./AiModule.js";
+import KI from "../KI.js";
 
 export default class ModDefend extends AiModule {
+	// TODO store ai in this
 	constructor() {
 		super();
 	}
 
+
+	/**
+	 * [TODO:description]
+	 *
+	 * @param {KI} ai - AI instance that is modulated
+	 */
 	modStep(ai) {
 		let bubbles = ai.getBubbles();
 
 		// Check all own bubbles if they need to be defended
 		for (let i = 0; i < bubbles.length; i++) {
 			let bubble = bubbles[i];
+
+			// TODO check endless loop → if bubbles contains only bubble
 
 			// Send support from random other own bubble
 			if (bubble.getArrivingEnemy() >= bubble.units + bubble.arriving[bubble.team]) {
@@ -21,7 +31,12 @@ export default class ModDefend extends AiModule {
 				if (randBubble === bubble)
 					continue;
 
+				if (!randBubble.createQueue.isEmpty()) {
+					continue;
+				}
+
 				ai.angriff(randBubble, bubble);
+				// break
 			}
 		}
 	}
